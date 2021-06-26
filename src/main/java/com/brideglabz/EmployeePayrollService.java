8eeb49cd9f4ba1df0a3355ed0c8195537c2818a7
mohammedsalmannn <rmdsalman001@gmail.com>
2021-06-26 19:56:27 +0530

    Added : Storin data into a txt file

--- src/main/java/com/brideglabz/EmployeePayrollService.java
+++ src/main/java/com/brideglabz/EmployeePayrollService.java
@@ -1,44 +1,56 @@
 package com.brideglabz;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class EmployeePayrollService {
-    public enum IOService{
-        CONSOLE_IO,FILE_IO,DB_IO,REST_IO
-    }
-    private List<EmployeePayrollData> employeePayrolllist;
-
-    public EmployeePayrollService(){
-
+    public enum IOService {
+        CONSOLE_IO, FILE_IO, DB_IO, REST_IO
     }
 
-    public EmployeePayrollService(List<EmployeePayrollData> employeePayrolllist) {
-        this.employeePayrolllist = employeePayrolllist;
+    private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+    public EmployeePayrollService() {}
+
+    public EmployeePayrollService(List<EmployeePayrollData> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee PlayRoll Service");
-        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<EmployeePayrollData>();
+        ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
         Scanner consoleInputReader = new Scanner(System.in);
         employeePayrollService.readEmployeePayrollData(consoleInputReader);
-        employeePayrollService.writeEmployeePayrollData();
+        employeePayrollService.writeEmployeeData(IOService.CONSOLE_IO);
 
     }
 
-    private void readEmployeePayrollData(Scanner consoleInputReader){
+    private void readEmployeePayrollData(Scanner consoleInputReader) {
         System.out.println("Enter Employee ID : ");
         int id = consoleInputReader.nextInt();
         System.out.println("enter Employee Name : ");
         String name = consoleInputReader.next();
         System.out.println("Enter Employee Salary : ");
         double salary = consoleInputReader.nextDouble();
-        employeePayrolllist.add(new EmployeePayrollData(id,name,salary));
+        employeePayrollList.add(new EmployeePayrollData(id, name, salary));
     }
 
-    private void writeEmployeePayrollData(){
-        System.out.println("Writing Employee Payroll Roaster to Console\n" + employeePayrolllist);
+    public void writeEmployeeData(IOService ioService) {
+        if (ioService.equals(IOService.CONSOLE_IO))
+            System.out.println("Writing Employee Payroll Roster in Console\n" + employeePayrollList);
+        else if (ioService.equals(IOService.FILE_IO)) {
+            EmployeePayrollFileIOService.writeData(employeePayrollList);
+        }
+
+    }
+
+    public long countEntries(IOService ioService) {
+        if (ioService.equals(IOService.CONSOLE_IO))
+            return employeePayrollList.size();
+        else if (ioService.equals(IOService.FILE_IO))
+            return EmployeePayrollFileIOService.countEntries();
+        return 0;
     }
 }
--- src/test/java/com/brideglabz/NIOFileAPITest.java
+++ src/test/java/com/brideglabz/NIOFileAPITest.java
@@ -1,25 +1,25 @@
-package com.brideglabz;
-import org.junit.Assert;
+package com.brideglabz;import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class NIOFileAPITest {
-    private static String HOME = System.getProperty("C:\\Users\\SalmaN\\IdeaProjects\\FilesCreated");
+    private static String HOME = System.getProperty("C:\\Users\\Durga Prasath\\IdeaProjects\\FilesCreated");
     private static String PLAY_WITH_NIO = "TempPlayGround";
 
     @Test
     public void givenPath_WhenChecked_ThenConfirm() throws IOException {
         //Check File Exists
-        Path homePath = Paths.get("C:\\Users\\SalmaN\\IdeaProjects\\FilesCreated");
+        Path homePath = Paths.get("C:\\Users\\Durga Prasath\\IdeaProjects\\FilesCreated");
         Assert.assertTrue(Files.exists(homePath));
 
         //Delete file and Check File Not Exist
-        Path playPath = Paths.get("C:\\Users\\SalmaN\\IdeaProjects\\FilesCreated\\PLAY_WITH_NIO");
+        Path playPath = Paths.get("C:\\Users\\Durga Prasath\\IdeaProjects\\FilesCreated\\PLAY_WITH_NIO");
         if (Files.exists(playPath)) FileUtils.deleteFiles(playPath.toFile());
         Assert.assertTrue(Files.notExists(playPath));
 
@@ -43,5 +43,18 @@ public class NIOFileAPITest {
         Files.list(playPath).filter(Files::isRegularFile).forEach(System.out::println);
         Files.newDirectoryStream(playPath).forEach(System.out::println);
         Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp")).forEach(System.out::println);
+    }
+
+    @Test
+    public void numberOfEmployeeEntryTest() {
+        EmployeePayrollData[] empArray = {
+                new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Mark Zuckerberg", 300000.0)
+        };
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService(Arrays.asList(empArray));
+        employeePayrollService.writeEmployeeData(EmployeePayrollService.IOService.FILE_IO);
+        long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+        Assert.assertEquals(3, entries);
     }
 }
