a9df729fcb088085110cb6f115a21b48d9d33887
mohammedsalmannn <rmdsalman001@gmail.com>
2021-06-28 10:27:31 +0530

    Added : TestCase to Retrive employee data and match the count

--- src/test/java/com/brideglabz/NIOFileAPITest.java
+++ src/test/java/com/brideglabz/NIOFileAPITest.java
@@ -88,5 +88,11 @@ public class NIOFileAPITest {
         Assert.assertEquals(3,entries);
     }
 
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        Assert.assertEquals(3,employeePayrollData.size());
+    }
 
 }
